4aab2fb51989013fb331a1b0c01f2650b15f28f9
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-29 11:43:07 +0530

    dcd

---
+++ 3.pushFrontArray/on.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int add_front(int a[],int n,int data)
+{
+    int i;
+    for(i=n-1;i>=0;i--)
+    {
+       a[i+1]=a[i];
+    }
+    a[0]=data;
+    return n+1;
+}
+int main()
+{
+    int n,i,freepos,data=8;
+    cout<<"Enter the number of element: "<<" "<<endl;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    n=add_front(a,n,data);
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
