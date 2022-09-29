7bc0ff40a2352c947323b0ad62e636386f321828
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-29 13:18:24 +0530

    bnmbmn

---
+++ 4.pushCertain/on.cpp
@@ -0,0 +1,27 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#define size 5
+using namespace std;
+
+int main() {
+    int a[size]={4,9,87,8,6};
+    int pos,i,element;
+    cout<<"Enter the position:"<<endl;
+    cin>>pos;
+    cout<<"Enetr the element you want: "<<endl;
+    cin>>element;
+    if(pos<=size && pos>=0)
+    {
+        for(i=size;i>pos;i--)
+        a[i]=a[i-1];
+        a[pos]=element;
+        for(i=0;i<element;i++)
+        
+            cout<<a[i];
+        }
+        else
+        cout<<"Invalid position";
+    
+
+    return 0;
+}
