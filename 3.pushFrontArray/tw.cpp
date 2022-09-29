1d1c651d39f5c9438f9bf12662731c4465696353
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-29 11:57:59 +0530

    vjb

---
+++ 3.pushFrontArray/tw.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int add_front(int a[],int b[],int size,int freepos,int data)
+{
+    int i,n;
+    for(i=0;i<n;i++)
+    {
+        b[i]=a[i];
+    }
+    b[freepos]=data;
+    freepos++;
+    return freepos;
+}
+int main()
+{
+    int n,i,size,freepos,data=9;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    if(n==size)
+    {
+       int b[size+2];
+        freepos=add_front(a,b,size,freepos,data);
+        for(i=0;i<freepos;i++)
+        {
+            cout<<b[i];
+        }
+       
+    }
+   
+    
+    return 0;
+}
