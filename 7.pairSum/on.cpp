7187b5fb20b4fc13bb18f6c1a43951f0b40f2e41
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-30 22:01:15 +0530

    bjbj

---
+++ 7.pairSum/on.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,j,key;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cin>>key;
+    for(i=0;i<n;i++)
+    {
+       
+        if(a[i]+a[i+1]==key)
+        {
+            cout<<a[i]<<" "<<a[i+1];
+        }
+       
+    }
+    return 0;
+}
