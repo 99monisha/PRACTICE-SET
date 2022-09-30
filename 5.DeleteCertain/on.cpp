8095e060af4e10ed94ab1647b89fbe856170fa2b
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-30 12:32:17 +0530

    fbvnrfb

---
+++ 5.DeleteCertain/on.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,pos;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"Enetr pos:"<<endl;
+    cin>>pos;
+    for(i=pos;i<n;i++)
+    {
+        a[i]=a[i+1];
+    }
+    for(i=0;i<n-1;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
