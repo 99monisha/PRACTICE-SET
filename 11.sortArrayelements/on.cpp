aa92d7ff5f39fbe97187346508b27f8707188041
Monisha Dolai <monishadolai1998@gmail.com>
2022-10-01 20:19:16 +0530

    bnbn

---
+++ 11.sortArrayelements/on.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+void sorting(int a[],int n)
+{
+    int i,j;
+     for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+   
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+int main()
+{
+    int i,n;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sorting(a,n);
+  
+    return 0;
+}
