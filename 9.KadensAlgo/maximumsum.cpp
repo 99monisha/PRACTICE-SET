1cfd3288114d8fb145e790e811e4b78461a76735
Monisha Dolai <monishadolai1998@gmail.com>
2022-10-01 12:52:50 +0530

    kadaens

---
+++ 9.KadensAlgo/maximumsum.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <climits>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int maxsum=INT_MIN;
+    int currentsum=0;
+    for(i=0;i<n;i++)
+    {
+        currentsum+=a[i];
+        while(currentsum<0)
+        {
+            currentsum=0;
+        }
+        
+       maxsum=max(maxsum,currentsum); 
+    }
+    cout<<"The maxium sum is: "<<maxsum;
+    return 0;
+}
