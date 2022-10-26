d22db224e6243c005c7ba7fc35df10d892a0017e
Monisha Dolai <monishadolai1998@gmail.com>
2022-10-27 00:12:53 +0530

    Occurrence of a digit in a given number

--- 35.Pro/on.cpp
+++ 35.Pro/on.cpp
@@ -7,23 +7,18 @@ Occurrence of a digit in a given number
 #include <iostream>
 using namespace std;
 int main() {
-
     int n = 890190798; 
     int d = 9; 
-
     int count=0; 
-
     while(n) {
-
-        int rem = n%10; 
-        if(rem == d){
-            count++;
-        }
+    int rem = n%10; 
+    if(rem == d)
+    {
+    count++;
+    }
         n=n/10; 
     }
-
     cout<<count;
-
     return 0;
 
 }
