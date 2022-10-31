2dd76ebff91cdd0835e4c72040758ffcfca13d99
Monisha Dolai <monishadolai1998@gmail.com>
2022-10-31 22:28:21 +0530

    jhnmb

---
+++ 37.Pro/tw.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main ()
+{
+  int num = 20901;
+  int digit = floor (log10 (num)) + 1;
+  cout << "No. of digits = " << digit;
+
+ }
